e478b9023b5011293bec78ff04b6fa7b198c9603
Zexi Jesse Zhuang <zhzexi@gmail.com>
2016-10-04 12:34:33 -0700

    hashmap mutablekey, map as key; concurrency unsafe sequence; pom concurrency in practice annotations

---
+++ src/main/java/org/incodelearning/concurrency/UnsafeSequence.java
@@ -0,0 +1,21 @@
+package org.incodelearning.concurrency;
+
+import net.jcip.annotations.*;
+
+/**
+ * UnsafeSequence
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+
+@NotThreadSafe
+public class UnsafeSequence {
+    private int value;
+
+    /**
+     * Returns a unique value.
+     */
+    public int getNext() {
+        return value++;
+    }
+}
